7e23ecdb0d8c7d5453689b0e4b868f547cc65a94
Omyim17 <86036284+Omyim17@users.noreply.github.com>
2021-06-18 22:09:56 +0700

    Update mine-hybrid.sh

--- mine-hybrid.sh
+++ mine-hybrid.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDWx2Sa8iyeLVTMUUi5ypY357ZXH5tNf56.hellminer -p hybrid --cpu 2
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWiK7SBz2gyjywBgAKCAyLS3XrPRTckJvY.OMYIM -p hybrid --cpu 2
